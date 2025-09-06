65acc83ba6412d8a12c7b2825e2286e97fb3d0ff
Zathura160801 <adipz160801@gmail.com>
2025-09-07 03:12:40 +0800

    Slicing Sidebar Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/layouts/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,9 @@ export default function RootLayout({
           <div className="border-t">
             <div className="bg-background">
               <div className="flex flex-row">
-                <div className="hidden lg:block w-[18%]">Sidebar</div>
+                <div className="hidden lg:block w-[18%]">
+                  <Sidebar />
+                </div>
                 <div className="col-span-3 overflow-auto lg:col-span-5 lg:border-l w-[82%]">
                   <div className="px-6 py-6 lg:px-8">
                     <div>Header</div>
